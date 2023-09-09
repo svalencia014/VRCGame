0c8b38aac960bc4c5256ad9376747d3da9e16044
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-09-09 00:55:28 -0400

    start rework

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace VrcGame
+{
+  public class Program
+  {
+    public static FSDServer fsdServer;
+    public static void Main()
+    {
+      
+    }
+
+    private void Initalize() 
+    {
+      Console.WriteLine("Initalizing...");
+
+    }
+  }
+}
---
+++ Server/FSDServer.cs
@@ -0,0 +1,11 @@
+using System.Net;
+using System.Net.Sockets;
+
+namespace VrcGame
+{
+  public class FSDServer
+  {
+    private static TcpClient? Client;
+    private static NetworkStream Stream;
+  }
+}
